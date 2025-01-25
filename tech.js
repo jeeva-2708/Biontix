84798f987736247f6a63702f3a57c4fd08945310
jeeva <jeeva.v6499@gmail.com>
2025-01-26 02:09:39 +0530

    adding monopoly page

--- tech.js
+++ tech.js
@@ -1,7 +1,7 @@
 const navi = document.getElementById("navi");
 const list = document.getElementById("list");
 const cross = document.getElementById("cross");
-const sec = document.getElementById("sec");
+// const sec = document.getElementById("sec");
 const times = document.querySelectorAll(".times");
 const reDirect = document.querySelectorAll(".redirect")
 const boxOne = document.getElementById("box-one")
@@ -20,38 +20,6 @@ window.addEventListener('scroll', function() {
 
 
 
-// reDirect to next page
-
-    boxOne.addEventListener("click", function(){
-        window.location.href = './technical-events.html';
-    })
-
-
-    boxTwo.addEventListener("click", function(){
-        window.location.href = './non-technical-events.html';
-    })
-
-// hover effect
-boxOne.addEventListener("mouseover",()=>{
-    overOne.style.transition="fontSize 5s linear"
-    overOne.style.fontSize="26px"
-    
-})
-boxOne.addEventListener("mouseout",()=>{
-    overOne.style.fontSize="20px"
-    overOne.style.transition="fontSize 0.5s linear"
-})
-boxTwo.addEventListener("mouseover",()=>{
-    overTwo.style.transition="fontSize 5s linear"
-    overTwo.style.fontSize="26px"
-    
-})
-boxTwo.addEventListener("mouseout",()=>{
-    overTwo.style.fontSize="20px"
-    overTwo.style.transition="fontSize 0.5s linear"
-})
-
-
 // navigation
 navi.addEventListener("click", ()=>{
     list.style.display="block";
@@ -65,13 +33,7 @@ cross.addEventListener("click", ()=>{
     list.style.display="none"
     navi.style.display="block"
 })
-sec.addEventListener("click", ()=>{
-     list.style.display="none"
-    navi.style.display="block"
-})
-
-
-    
-    
-   
-   
+// sec.addEventListener("click", ()=>{
+//      list.style.display="none"
+//     navi.style.display="block"
+// })
